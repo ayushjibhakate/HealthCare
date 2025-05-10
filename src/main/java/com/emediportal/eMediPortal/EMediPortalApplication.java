24b714d1a56da9a67bcab50998e0117fabc3d50a
hp <hp@DESKTOP-2TC7F7M.lan>
2025-05-10 14:58:01 +0530

    commite

---
+++ src/main/java/com/emediportal/eMediPortal/EMediPortalApplication.java
@@ -0,0 +1,13 @@
+package com.emediportal.eMediPortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EMediPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EMediPortalApplication.class, args);
+	}
+
+}
